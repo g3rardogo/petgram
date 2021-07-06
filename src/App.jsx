bc06446e63328f744d8d28eddb2c8a0b0a9df6f5
Gerardo Ramirez <gerardogo145@gmail.com>
2021-07-06 02:36:29 +0200

    :art: Add protected routes

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,14 @@ import { Logo } from "./components/Logo";
 import { PhotoCardWithQuery } from "./containers/PhotoCardWithQuery";
 import { Home } from "./pages/Home";
 import { Detail } from "./pages/Detail";
+import { Favs } from "./pages/Favs";
+import { User } from "./pages/User";
+import { NotRegisteredUser } from "./pages/NotRegisteredUser";
 import { Navbar } from "./components/NavBar";
 
+const UserLogged = ({ children }) => {
+  return children({ isAuth: false });
+};
 export const App = () => {
   return (
     <>
@@ -18,6 +24,21 @@ export const App = () => {
           <Route exact path="/pet/:categoryId" component={Home} />
           <Route exact path="/detail/:detailId" component={Detail} />
         </Switch>
+        <UserLogged>
+          {({ isAuth }) =>
+            isAuth ? (
+              <Switch>
+                <Route exact path="/favs" component={Favs} />
+                <Route exact path="/user" component={User} />
+              </Switch>
+            ) : (
+              <Switch>
+                <Route exact path="/favs" component={NotRegisteredUser} />
+                <Route exact path="/user" component={NotRegisteredUser} />
+              </Switch>
+            )
+          }
+        </UserLogged>
         <Navbar />
       </BrowserRouter>
     </>
