6d6320c490e2c12565361ef6664ec5b00b70d00d
Gerardo Ramirez <gerardogo145@gmail.com>
2021-07-10 22:01:05 +0200

    :mag: Improve SEO with React Helmet

--- src/pages/Favs.jsx
+++ src/pages/Favs.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { GetFavorites } from "../containers/GetFavorites";
+import { Helmet } from "react-helmet";
+import { Layout } from "../components/Layout";
 
 export const Favs = () => {
   return (
-    <>
-      <h1>Favs</h1>
+    <Layout title="Tus favoritos" subtitle="Aqui encuentras tus favoritos">
       <GetFavorites />
-    </>
+    </Layout>
   );
 };
